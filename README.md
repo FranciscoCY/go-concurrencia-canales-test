dd4c322656261efda8c30b75f73b8221859134ef
Francisco F. Cabanillas Yali <frank97-1@outlook.com>
2023-02-20 11:13:29 -0500

    unit test and coverager

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # go-concurrencia-canales-test
 Proyecto donde se utiliza los conceptos concurrencia, canales y testing
+
+## Tech Stack  
+**Client:** Go, Golang  
+
+**Server:** Golang  
+ 
+## Run Locally  
+Unit Test
+
+~~~bash  
+  go test
+~~~
+
+Coverage Unit test
+
+~~~bash  
+  go test -cover
+~~~
+
+Report coverage
+~~~bash
+ go test -coverprofile=coverage.out
+~~~
+
+Report coverage
+~~~bash
+ go tool cover -func=coverage.out
+~~~
+
+Report coverage html
+~~~bash
+ go tool cover -html=coverage.out
+~~~
+
+Report coverage - time
+~~~bash
+ go test -cpuprofile=cou.out
+~~~
+
+~~~bash
+ go tool pprof cou.out
+~~~
--- testing/testunitario/mate_test.go
+++ testing/testunitario/mate_test.go
@@ -9,3 +9,62 @@ func TestSuma(t *testing.T) {
 		t.Errorf("Suma incorrecta, tiene %d se esperaba %d", total, 10)
 	}
 }
+
+func TestSumaTable(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	}{
+		{1, 2, 3},
+		{2, 2, 4},
+		{25, 25, 50},
+	}
+
+	for _, item := range tabla {
+		total := Suma(item.a, item.b)
+
+		if total != item.c {
+			t.Errorf("Suma incorrecta, tiene %d se esperaba %d", total, item.c)
+		}
+	}
+}
+
+func TestGetMax(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	}{
+		{4, 2, 4},
+		{5, 2, 5},
+		{2, 3, 3},
+	}
+
+	for _, item := range tabla {
+		max := GetMax(item.a, item.b)
+
+		if max != item.c {
+			t.Errorf("GetMax incorrecta, tiene %d se esperaba %d", max, item.c)
+		}
+	}
+}
+
+func TestFib(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+	}{
+		{1, 1},
+		{8, 21},
+		{50, 12586269025},
+	}
+
+	for _, item := range tabla {
+		fibo := Fibonacci(item.a)
+
+		if fibo != item.b {
+			t.Errorf("GetMax incorrecta, tiene %d se esperaba %d", fibo, item.b)
+		}
+	}
+}
